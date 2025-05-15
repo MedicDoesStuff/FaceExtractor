673fcd0b91aebb39244a191e35f0b78dbcd435b7
MedicDoesStuff <paddington2sucks@gmail.com>
2025-05-15 15:05:03 +0000

    Add contents of core folder (including subfolders) to dist/core

---
+++ dist/core/lib/image/gen/__init__.py
@@ -0,0 +1,7 @@
+"""
+Various image generators.
+"""
+
+from ._gen import (bezier, binary_clouds, binary_stripes, circle_faded, clouds,
+                   cut_edges_mask, icon_loading, noise, patch_dropout_mask,
+                   stripes, test_gen)
