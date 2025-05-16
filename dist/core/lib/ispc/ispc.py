0b08267109c0253eed5095df4e1b129611522cfb
Medic <71578066+MedicDoesStuff@users.noreply.github.com>
2025-05-16 12:48:22 +0800

    Update ispc.py

--- dist/core/lib/ispc/ispc.py
+++ dist/core/lib/ispc/ispc.py
@@ -8,7 +8,8 @@ from ctypes.util import find_library
 from pathlib import Path
 from typing import get_type_hints
 
-import ziglang
+# Hardcode zig_bin path for Colab
+zig_bin = Path('/content/zig/zig')
 
 c_float_p = c_void_p
 c_uint8_p = c_void_p
@@ -54,101 +55,58 @@ def dll_import(dll_name):
         return wrapper
     return decorator
 
-
-zig_bin = Path(ziglang.__file__).parent / 'zig'
-
+# Set ISPC_PATH programmatically
 if (ispc_root := os.environ.get('ISPC_PATH', None)) is not None:
     ispc_bin = Path(ispc_root) / 'bin' / 'ispc'
 else:
-    raise Exception('environ variable ISPC_PATH is not specified')
+    ispc_bin = Path('/content/ispc/bin/ispc')  # Fallback for Colab
 
-def cc(args, cwd : Path):
-    args = [ str(zig_bin), 'c++' ] + args
-    p = subprocess.Popen(args, stdout=subprocess.DEVNULL,  stderr=subprocess.PIPE, cwd=str(cwd))
+def cc(args, cwd: Path):
+    args = [str(zig_bin), 'c++'] + args
+    p = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=str(cwd))
     out, err = p.communicate()
     if p.returncode != 0:
         err = err.decode('utf-8') if err is not None else None
         raise Exception(err)
 
-
-def compile_o(ispc_file : Path):
+def compile_o(ispc_file: Path):
     """
     compile .o from .ispc file.
-
     raise on error
     """
     cwd = ispc_file.parent
-
-    shared_lib_path = ispc_file.parent / (ispc_file.stem+get_shared_lib_suffix())
-
-    ispc_h_filepath = ispc_file.parent / (ispc_file.stem+'.h')
-    # ispc_tmp_c_filepath = ispc_file.parent / (ispc_file.stem+'_tmp.c')
-    # ispc_tmp_lib_filepath = ispc_file.parent / (ispc_file.stem+'_tmp.lib')
-    ispc_o_filepath = ispc_file.parent / (ispc_file.stem+'.o')
-
+    shared_lib_path = ispc_file.parent / (ispc_file.stem + get_shared_lib_suffix())
+    ispc_h_filepath = ispc_file.parent / (ispc_file.stem + '.h')
+    ispc_o_filepath = ispc_file.parent / (ispc_file.stem + '.o')
     include_root = Path(__file__).parent / 'include'
 
-    args = [ str(ispc_bin), str(ispc_file), '-o', str(ispc_o_filepath),
-            '-h', str(ispc_h_filepath), '-I', str(include_root), #'--opt=fast-math',
-            '--pic',
-            #'--werror',
-
-              ]
+    args = [str(ispc_bin), str(ispc_file), '-o', str(ispc_o_filepath),
+            '-h', str(ispc_h_filepath), '-I', str(include_root), '--pic']
 
     p = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=str(cwd))
-
     out, err = p.communicate()
 
     if p.returncode != 0:
         err = err.decode('utf-8') if err is not None else None
         raise Exception(err)
 
-#     ispc_tmp_c_filepath.write_text(
-# fr"""#include "{ispc_h_filepath.name}"
-# extern int _fltused = 0;
-# extern size_t __chkstk(size_t size)
-# {{
-#     __asm__(
-#         "movq $0x0, %r11 \r\n"
-#         "movl $0xFFFFFFFF, %r11d \r\n"
-#         "and %r11, %rax \r\n"
-#         "retn \r\n"
-#     );
-# }}
-# """)
-
-    #ispc_h_filepath.unlink()
-    #ispc_o_filepath.unlink()
-    # ispc_tmp_c_filepath.unlink()
-    # ispc_tmp_lib_filepath.unlink()
-
-
-def compile_dll(ispc_file : Path):
+def compile_dll(ispc_file: Path):
     """
     compile dll from .ispc file.
-
     raise on error
     """
     cwd = ispc_file.parent
-
-    shared_lib_path = ispc_file.parent / (ispc_file.stem+get_shared_lib_suffix())
-
-    ispc_h_filepath = ispc_file.parent / (ispc_file.stem+'.h')
-    ispc_tmp_c_filepath = ispc_file.parent / (ispc_file.stem+'_tmp.c')
-    ispc_tmp_lib_filepath = ispc_file.parent / (ispc_file.stem+'_tmp.lib')
-    ispc_o_filepath = ispc_file.parent / (ispc_file.stem+'.o')
-
+    shared_lib_path = ispc_file.parent / (ispc_file.stem + get_shared_lib_suffix())
+    ispc_h_filepath = ispc_file.parent / (ispc_file.stem + '.h')
+    ispc_tmp_c_filepath = ispc_file.parent / (ispc_file.stem + '_tmp.c')
+    ispc_tmp_lib_filepath = ispc_file.parent / (ispc_file.stem + '_tmp.lib')
+    ispc_o_filepath = ispc_file.parent / (ispc_file.stem + '.o')
     include_root = Path(__file__).parent / 'include'
 
-    args = [ str(ispc_bin), str(ispc_file), '-o', str(ispc_o_filepath),
-            '-h', str(ispc_h_filepath), '-I', str(include_root), #'--opt=fast-math',
-            '--pic',
-            #'--werror',
-
-              ]
+    args = [str(ispc_bin), str(ispc_file), '-o', str(ispc_o_filepath),
+            '-h', str(ispc_h_filepath), '-I', str(include_root), '--pic']
 
     p = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=str(cwd))
-
     out, err = p.communicate()
 
     if p.returncode != 0:
@@ -156,7 +114,7 @@ def compile_dll(ispc_file : Path):
         raise Exception(err)
 
     ispc_tmp_c_filepath.write_text(
-fr"""#include "{ispc_h_filepath.name}"
+        fr"""#include "{ispc_h_filepath.name}"
 extern int _fltused = 0;
 extern size_t __chkstk(size_t size)
 {{
@@ -169,13 +127,12 @@ extern size_t __chkstk(size_t size)
 }}
 """)
 
-    args = ['-g0', '-flto=thin',  '-O3',
+    args = ['-g0', '-flto=thin', '-O3',
             '-fno-unwind-tables', '-fno-exceptions',
             '-nodefaultlibs', '-nostdinc', '-nostdinc++',
-            '-fno-pch-codegen','-fno-pch-debuginfo', '-Wl,--strip-debug',
+            '-fno-pch-codegen', '-fno-pch-debuginfo', '-Wl,--strip-debug',
             '-shared', '-o', str(shared_lib_path),
-            str(ispc_tmp_c_filepath), str(ispc_o_filepath),
-            ]
+            str(ispc_tmp_c_filepath), str(ispc_o_filepath)]
     cc(args, cwd=cwd)
 
     ispc_h_filepath.unlink()
@@ -183,9 +140,9 @@ extern size_t __chkstk(size_t size)
     ispc_tmp_c_filepath.unlink()
     ispc_tmp_lib_filepath.unlink()
 
-_os_shlib_suffix = {'Windows' : '.dll',
-                    'Linux' : '.so',
-                    'Darwin' : '.dylib', }
+_os_shlib_suffix = {'Windows': '.dll',
+                    'Linux': '.so',
+                    'Darwin': '.dylib'}
 
 def get_shared_lib_suffix() -> str:
     suffix = _os_shlib_suffix.get(platform.system(), None)
